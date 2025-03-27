f263bde827b9f99c0c110f5431c7b70f14c3bf8d
Natsubrei <natsunobourei@163.com>
2025-03-27 20:14:01 +0800

    Update RequestHandler

--- src/main/java/handler/RequestHandler.java
+++ src/main/java/handler/RequestHandler.java
@@ -5,28 +5,24 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import store.StoreFactory;
 
-import java.util.concurrent.ConcurrentHashMap;
-
 @ChannelHandler.Sharable
 public class RequestHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        ConcurrentHashMap<String, String> map = StoreFactory.getMap();
-
         String command = (String) msg;
-        String[] s = command.split(" ");
         // log.debug("command: {}", command);
+        String[] s = command.split(" ");
 
         switch (s[0]) {
             case "PUT":
-                map.put(s[1], s[2]);
+                StoreFactory.getMap().put(s[1], s[2]);
                 ctx.writeAndFlush("OK\n");
                 break;
             case "GET":
-                ctx.writeAndFlush(map.get(s[1]) + '\n');
+                ctx.writeAndFlush(StoreFactory.getMap().get(s[1]) + '\n');
                 break;
             case "DELETE":
-                map.remove(s[1]);
+                StoreFactory.getMap().remove(s[1]);
                 ctx.writeAndFlush("OK\n");
                 break;
             case "EXIT":
