3fa5158ca6b53b78f3863cd60d9b1a420be8f707
Natsubrei <natsunobourei@163.com>
2025-03-27 17:47:24 +0800

    Update RequestHandler

--- src/main/java/handler/RequestHandler.java
+++ src/main/java/handler/RequestHandler.java
@@ -11,22 +11,23 @@ import java.util.concurrent.ConcurrentHashMap;
 public class RequestHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        String command = (String) msg;
-        // log.debug("command: {}", command);
-        String[] s = command.split(" ");
         ConcurrentHashMap<String, String> map = StoreFactory.getMap();
-        String OPERATE_SUCCESS = "OK\n";
+
+        String command = (String) msg;
+        String[] s = command.split(" ");
+        // log.debug("command: {}", command);
+
         switch (s[0]) {
             case "PUT":
                 map.put(s[1], s[2]);
-                ctx.writeAndFlush(OPERATE_SUCCESS);
+                ctx.writeAndFlush("OK\n");
                 break;
             case "GET":
                 ctx.writeAndFlush(map.get(s[1]) + '\n');
                 break;
             case "DELETE":
                 map.remove(s[1]);
-                ctx.writeAndFlush(OPERATE_SUCCESS);
+                ctx.writeAndFlush("OK\n");
                 break;
             case "EXIT":
                 ctx.channel().close();
