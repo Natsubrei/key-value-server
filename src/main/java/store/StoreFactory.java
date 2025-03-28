aa2b656ee08c8f1b4df873d792172401fe2c3ac6
Natsubrei <natsunobourei@163.com>
2025-03-28 17:45:27 +0800

    Update StoreFactory

--- src/main/java/store/StoreFactory.java
+++ src/main/java/store/StoreFactory.java
@@ -4,6 +4,9 @@ import lombok.Getter;
 
 import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * 工厂类，用于提供线程安全的键值存储容器
+ */
 public abstract class StoreFactory {
     @Getter
     public static ConcurrentHashMap<String, String> map = new ConcurrentHashMap<>();
